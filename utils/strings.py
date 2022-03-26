7806e70f1230957c7ae0daea9fde13bc20f9a9f2
notchia <lucia.korpas@gmail.com>
2022-03-26 12:46:47 -0500

    draft useful string utils

---
+++ utils/strings.py
@@ -0,0 +1,25 @@
+import re
+
+
+def camel_to_snake(s):
+    return s
+
+
+def snake_to_camel(s, upper=False):
+    return s
+
+
+def camel_to_plain(s):
+    return s
+
+
+def snake_to_plain(s):
+    return s
+
+
+def plain_to_camel(s, upper=False):
+    return s
+
+
+def plain_to_snake(s):
+    return s

95c5baedfe7833ab927db3a0d57fc3256061f06c
notchia <lucia.korpas@gmail.com>
2022-04-20 14:29:58 -0500

    add basic view of factories

--- daochem/database/urls.py
+++ daochem/database/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:contract_address>/', views.factory_contract_summary, name='factory_contract_summary'),
 ]
--- daochem/database/views.py
+++ daochem/database/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Q
+from daochem.database.models.blockchain import DaoFactory
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the DAOs index.")
 
-# Create your views here.
+
+def factory_contract_summary(request, contract_address):
+    fac = DaoFactory.objects.get(Q(contract_address=contract_address))
+    r_lines = []
+    r_lines.append(f"These are the results for the factory contract {fac.contract_address.contract_name} for {fac.dao_framework.name} version {fac.version} at {contract_address}")
+    totalTransactions = fac.related_transactions.count()
+    r_lines.append(f"Number of contract-creating transactions: {totalTransactions}")
+    
+    return HttpResponse("\n\n".join(r_lines))

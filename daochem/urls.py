b1de6d42c8dbc184e64390c4363faad71a3e33f2
notchia <lucia.korpas@gmail.com>
2022-04-25 13:02:16 -0500

    update settings for togging local/production; fix url

--- daochem/urls.py
+++ daochem/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('daochem/database/', include('daochem.database.urls')),
+    path('', include('daochem.database.urls'))
 ]
